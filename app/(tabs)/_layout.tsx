ca630120291f10ac7118350299e0c93c45c8057b
CodeNameHealer <swagatprasad3344@gmail.com>
2025-06-21 03:37:15 +0530

    Fixed the styling and final updates

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -44,7 +44,7 @@ export default function TabLayout() {
       }}
     >
       <Tabs.Screen
-        name="index"
+        name="home"
         options={{
           headerTitle: () => (
             <Text
---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import "../global.css";
+
+export default function Home() {
+    return (
+      <View className="flex-1 items-center justify-center bg-red-100">
+        <Text className="text-blue-500 text-lg font-bold">Tailwind Test</Text>
+      </View>
+    );
+}  
