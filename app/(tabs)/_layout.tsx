37a6141a723d286d0ef9108fdd0f69a9bd5ce456
CodeNameHealer <swagatprasad3344@gmail.com>
2025-06-21 03:21:42 +0530

    Redirected to home

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -44,7 +44,7 @@ export default function TabLayout() {
       }}
     >
       <Tabs.Screen
-        name="home"
+        name="index"
         options={{
           headerTitle: () => (
             <Text
